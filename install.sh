b75ac685c6c209b9902fc31d2d132fe00995cb74
Velcon-Zheng <31822654+Velcon-Zheng@users.noreply.github.com>
2022-04-27 23:05:34 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+read -p "Enter Your trash folder: " TrashFolder
+read -p "Enter Your Software bin folder: " BinFolder
+echo -e "\nYour trash folder is $TrashFolder!"
+echo "Your Software bin folder is $BinFolder!"
+
+#Step1
+sed -i "24d"  rt
+sed -i "23a alias rm=\"$BinFolder/rt\""  rt
+echo "alias rm=\"$BinFolder/rt\""  >> ~/.bashrc
+sed -i "25d"  rt
+sed -i "24a Trashdir=\"$TrashFolder\""  rt 
+
+#step2
+cp rt $BinFolder
+
+#step3
+crontab -l > /tmp/crontab.$$
+echo "0 0 * * 0 rm -rf $TrashFolder/* " >> /tmp/crontab.$$  
+crontab /tmp/crontab.$$
+source ~/.bashrc 
+
+echo -e "\n\nGood jobs! \nInstallation of rt is finished! \n"
+
